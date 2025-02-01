25fe973c47f7dfc660dd27967d2ff4eea9a4f655
JoshAgustinT <89602882+JoshAgustinT@users.noreply.github.com>
2025-02-01 11:07:10 -0700

    Add files via upload

---
+++ profile.py
@@ -0,0 +1,27 @@
+"""
+DRAMhit
+
+
+Instructions:
+Nothing to do here, look into .sh if you would like
+"""	  
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+node_0 = request.RawPC('node-0')
+node_0.hardware_type = 'c6620'
+node_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
+
+# Install and execute a script that is contained in the repository.
+node_0.addService(pg.Execute(shell="sh", command="/local/repository/dramhit-top.sh"))
+
+# Print the generated rspec
+pc.printRequestRSpec(request)
